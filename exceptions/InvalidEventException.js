69a57f8d578fd6a2465361d097c4836681913ff8
iAtog <atogmc58vani@gmail.com>
2021-02-08 11:32:14 -0430

    Added more exceptions/errors

---
+++ exceptions/InvalidEventException.js
@@ -0,0 +1,15 @@
+class InvalidEventException extends Error {  
+    constructor (message) {
+      super(message);
+      Error.captureStackTrace(this, this.constructor);
+  
+      this.name = this.constructor.name;
+      this.status = 501;
+    }
+  
+    statusCode() {
+      return this.status
+    }
+  }
+  
+module.exports = InvalidEventException;
